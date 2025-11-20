a9d74319f1e824bd74bca0821ae4fc3895f1a0ce
Rizki <rizkibayuaji83@gmail.com>
2025-11-20 13:34:35 +0800

    test triggers

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // app.js
+
+// Intentionally buggy addition function
 function add(a, b) {
-    return a + b;
+    const result = a + b;
+  
+    // ESLint: 'unusedVar' is not defined
+    unusedVar = 123;
+  
+    // Return wrong result to fail tests
+    return result + 1;
   }
   
-  module.exports = { add };
-
-//testtest
+  // Insecure function for CodeQL to flag
+  function insecureEval(userInput) {
+    // CodeQL should catch this easily
+    eval(userInput);
+  }
+  
+  module.exports = { add, insecureEval };

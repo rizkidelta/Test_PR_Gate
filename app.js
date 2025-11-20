65e63b283381d2963cfe178a9bd44460a6c6bd7a
Rizki <rizkibayuaji83@gmail.com>
2025-11-20 13:21:41 +0800

    Revert

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
 // app.js
-
-// Intentionally buggy addition function
 function add(a, b) {
-    const result = a + b;
-  
-    // ESLint: 'unusedVar' is not defined (no-undef, no-implicit-globals)
-    unusedVar = 123;
-  
-    // Wrong result on purpose so tests fail
-    return result + 1;
+    return a + b;
   }
   
-  // Intentionally insecure function so CodeQL flags it
-  function insecureEval(userInput) {
-    // CodeQL should flag this as a potential code injection vulnerability
-    eval(userInput);
-  }
-  
-  module.exports = { add, insecureEval };
+  module.exports = { add };

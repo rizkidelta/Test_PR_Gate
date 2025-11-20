9815a2a4f3006d9b75588ffb34bf0625f8ea6a80
Rizki <rizkibayuaji83@gmail.com>
2025-11-20 11:39:09 +0800

    Addition

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,15 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    // Use Node, not browser
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" }
+  }
 ]);
